f95500f9637841ba6a8604188a232d7fe08d8018
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2020-02-07 19:21:18 +0800

    feat(inline-editor): update new version

--- src/javascript-inline-editor/src/ckeditor.js
+++ src/javascript-inline-editor/src/ckeditor.js
@@ -79,59 +79,6 @@ InlineEditor.builtinPlugins = [
 
 // Editor configuration.
 InlineEditor.defaultConfig = {
-	fontColor: {
-		colors: [
-			{
-				color: 'hsl(0, 0%, 0%)',
-				label: 'Black'
-			},
-			{
-				color: 'hsl(0, 0%, 30%)',
-				label: 'Dim grey'
-			},
-			{
-				color: 'hsl(0, 0%, 60%)',
-				label: 'Grey'
-			},
-			{
-				color: 'hsl(0, 0%, 90%)',
-				label: 'Light grey'
-			},
-			{
-				color: 'hsl(0, 0%, 100%)',
-				label: 'White',
-				hasBorder: true
-			},
-
-			// ...
-		]
-	},
-	fontBackgroundColor: {
-		colors: [
-			{
-				color: 'hsl(0, 75%, 60%)',
-				label: 'Red'
-			},
-			{
-				color: 'hsl(30, 75%, 60%)',
-				label: 'Orange'
-			},
-			{
-				color: 'hsl(60, 75%, 60%)',
-				label: 'Yellow'
-			},
-			{
-				color: 'hsl(90, 75%, 60%)',
-				label: 'Light green'
-			},
-			{
-				color: 'hsl(120, 75%, 60%)',
-				label: 'Green'
-			},
-
-			// ...
-		]
-	},
 	toolbar: {
 		items: [
 			'heading',
